d07b45b3f599f5f2db28a4de466b7bf9a1012f6b
Juss825 <jussefherguera2@gmail.com>
2026-03-02 21:48:08 -0600

    Update and rename electrico.h to camion.cpp
    Signed-off-by: Juss825 <jussefherguera2@gmail.com>

---
+++ retos/Reto 01 – Gestión de vehículos con herencia/camion.cpp
@@ -0,0 +1,14 @@
+#include "camion.h"
+#include <iostream>
+
+Camion::Camion(std::string marca, std::string modelo, int anio,
+               std::string placa, double capacidadCarga)
+    : Vehiculo(marca, modelo, anio, placa) {
+    this->capacidadCarga = capacidadCarga;
+}
+
+void Camion::mostrarInformacion() {
+    Vehiculo::mostrarInformacion();
+    std::cout << "Capacidad de carga: "
+              << capacidadCarga << " toneladas" << std::endl;
+}
