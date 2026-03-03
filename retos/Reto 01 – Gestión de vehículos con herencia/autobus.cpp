fcec7a451d86b5ef57e993a05c4af076cf3d33d9
Juss825 <jussefherguera2@gmail.com>
2026-03-02 21:50:17 -0600

    Create autobus.cpp
    Signed-off-by: Juss825 <jussefherguera2@gmail.com>

---
+++ retos/Reto 01 – Gestión de vehículos con herencia/autobus.cpp
@@ -0,0 +1,14 @@
+#include "autobus.h"
+#include <iostream>
+
+Autobus::Autobus(std::string marca, std::string modelo, int anio,
+                 std::string placa, int numeroPasajeros)
+    : Vehiculo(marca, modelo, anio, placa) {
+    this->numeroPasajeros = numeroPasajeros;
+}
+
+void Autobus::mostrarInformacion() {
+    Vehiculo::mostrarInformacion();
+    std::cout << "Numero de pasajeros: "
+              << numeroPasajeros << std::endl;
+}
